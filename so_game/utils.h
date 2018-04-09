d7976f857bb9320310aa0fb3adde273f4956a8e5
Andrea Antonini <alantonini96roma@gmail.com>
2018-04-09 16:31:36 +0200

    created utils.h for macros

---
+++ so_game/utils.h
@@ -0,0 +1,40 @@
+#include "image.h"
+#include <errno.h>
+
+
+#define SERVER_IP 	"127.0.0.1"
+#define SERVER_PORT 8080
+#define ACTIVE			8
+#define BUFFERSIZE	1000000
+
+//USED FOR DEBUG
+#define SERVER	"[SERVER] "
+#define CLIENT  "[CLIENT] "
+#define TCP		"[TCP] "
+#define UDP		"[UDP] "
+
+#define DEBUG	1
+
+
+
+#define GENERIC_ERROR_HELPER(cond, errCode, msg) do {               \
+        if (cond) {                                                 \
+            fprintf(stderr, "%s: %s\n", msg, strerror(errCode));    \
+            exit(EXIT_FAILURE);                                     \
+        }                                                           \
+    } while(0)
+
+
+#define ERROR_HELPER(ret, msg)          GENERIC_ERROR_HELPER((ret < 0), errno, msg)
+#define PTHREAD_ERROR_HELPER(ret, msg)  GENERIC_ERROR_HELPER((ret != 0), ret, msg)
+
+
+struct args{ //USED BY A THREAD IN SERVER
+	Image * surface_texture;
+	Image * elevation_texture;
+	Image * vehicle_texture;
+	int tcp_socket;
+	int udp_socket;
+	int idx;
+};
+
