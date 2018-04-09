c79aca7898ebbc3f38afdbb9fc30b0cf2bb3ad4e
Andrea Antonini <alantonini96roma@gmail.com>
2018-04-09 16:30:21 +0200

    started server side of videogame

--- so_game/so_game_server.c
+++ so_game/so_game_server.c
@@ -12,12 +12,17 @@
 #include "world_viewer.h"
 
 int main(int argc, char **argv) {
-  if (argc<3) {
-    printf("usage: %s <elevation_image> <texture_image>\n", argv[1]);
+  if (argc<4) {
+    printf("usage: %s <elevation_image> <texture_image> <port_number>\n", argv[1]);
     exit(-1);
   }
   char* elevation_filename=argv[1];
   char* texture_filename=argv[2];
+  long tmp = strtol(argv[3], NULL, 0);
+  if (tmp < 1024 || tmp > 49151) {
+	fprintf(stderr, "Use a port number between 1024 and 49151.\n");
+	exit(EXIT_FAILURE);
+  }
   char* vehicle_texture_filename="./images/arrow-right.ppm";
   printf("loading elevation image from %s ... ", elevation_filename);
 
@@ -45,6 +50,31 @@ int main(int argc, char **argv) {
   } else {
     printf("Fail! \n");
   }
+  
+  int ret, socket_tcp, socket_udp;			//network variables
+  struct sockaddr_in server_addr{0};
+  
+  if(DEBUG) printf("%s... initializing TCP\n", SERVER);
+  if(DEBUG) printf("%s...Socket creation\n", SERVER);
+  
+  //socket for TCP communication
+  socket_tcp = socket(AF_INET, SOCK_STREAM, 0);
+  ERROR_HELPER(socket_tcp, "Error in socket creation\n");
+  
+  server_addr.sin_family			= AF_INET;
+  server_addr.sin_port				= htons(SERVER_PORT);
+  server_addr.sin_addr.s_addr	= INADDR_ANY;
+  
+  int reuseaddr_opt = 1;		//recover a server in case of a crash
+  ret = setsockopt(server_tcp, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_opt, sizeof(reuseaddr_opt));
+  ERROR_HELPER(ret, "Failed setsockopt() on server socket_tcp");
+  
+  //binding
+  ret = bind(socket_tcp, (struct sockaddr*) &server_addr, sizeof(server_addr);
+  ERROR_HELPER(ret, "Error in binding\n");
+  
+  
+  
 
   // not needed here
   //   // construct the world
