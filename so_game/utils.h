31099e95d91b4d4f390ba721e7e558b5c457c570
Andrea Antonini <alantonini96roma@gmail.com>
2018-04-12 11:28:51 +0200

    Modified utils.h

--- so_game/utils.h
+++ so_game/utils.h
@@ -1,20 +1,17 @@
-typedef struct client_connected{
-	int id;
-	Image* texture;
-	sockaddr_in* addr; //to send data over udp socket to the client
-	int socket_TCP  //to send data over tcp to the client(la socket ricevuta dalla accept)
-}client_connected;
+typedef struct tcp_args{
+	int client_desc;
+	Image* elevation_texture;
+	Image* surface_elevation;
+} tcp_args;
 
 typedef struct thread_server_tcp_args{
     int socket_desc_tcp_client;
-    client_connected* connected;
-    client_disconnected* disconnected;
+    clientList* connected;
 }thread_server_tcp_args;
 
 typedef struct thread_server_udp_args{
     int socket_desc_udp_server;
-    client_connected* connected;
-    client_disconnected* disconnected;
+    clientList* connected;
 }thread_server_udp_args;
 
 typedef struct playerWorld{
